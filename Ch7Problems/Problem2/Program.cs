7fa5e3fd322d0a3bd4ef1a3340acfe6876ad235d
michtchakm <michaelmicht@gmail.com>
2013-09-10 08:42:42 -0400

    set 6 problems

---
+++ Ch7Problems/Problem2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem2
+{
+    class HousePlant
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Ch7Problems/Problem3/TestCircles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem3
+{
+    class TestCircles
+    {
+    }
+}
